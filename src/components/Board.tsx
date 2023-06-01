7e222c0df932296f69c769efbe63a178daf2a9c5
Lucas Suota <109081493+LucasSuota@users.noreply.github.com>
2023-05-31 21:33:04 -0300

    setting things up

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,3 @@
-import { useState, useEffect } from "react";
-
 interface Tool {
   name: string;
   thickness: number;
@@ -7,9 +5,21 @@ interface Tool {
 }
 
 export function Board(props: { currentTool: Tool }) {
+  const Canvas = () => {
+    return (
+      <div className="bg-white w-3/5 h-4/5 drop-shadow-sm">
+        <div className="bg-indigo-500 flex items-center justify-center">
+          <h1 className="font-medium">Tool: {props.currentTool.name}</h1>
+        </div>
+      </div>
+    );
+  };
+
   return (
-    <div className="absolute left-24 ">
-      <h2>{props.currentTool.name}</h2>
+    <div className="">
+      <div className="w-full h-[100vh] flex items-center justify-center bg-blue-200">
+        <Canvas />
+      </div>
     </div>
   );
 }
